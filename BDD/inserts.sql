38f9654b9fd2aef26d3fd1946b012115cd8d0d20
Javier Aso Burgos <asoycia@hotmail.com>
2020-11-02 20:43:30 +0100

    Primer commit de mi proyecto TFG, donde está creada y rellenada la BDD

---
+++ BDD/inserts.sql
@@ -0,0 +1,181 @@
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Aceite para quemar (frasco)','5 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Agua sagrada (frasco)','25 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Antorcha (1 hora de luz)','1 mc');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Cantimplora (2 litros)','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Espejo, pequeño de acero','20 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Cuerda común (15 metros)','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Carcaj y 20 flechas','8 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Estacas (4) y maza de madera','3 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Farol con capuchón (4h de luz)','7 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Farol de ojo de buey (4h de luz)','12 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Garfio de escalada','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Herramientas de ladrón','25 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Instrumento musical','5 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Libro de conjuros (50 hojas)','25 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Mochila (10 kg de capacidad)','5 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Palanca','2 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Pértiga de madera (3 metros)','2 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Piquetas de hierro (10)','5 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Raciones comunes (1 día)','5 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Raciones de viaje (1 día)','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Saco (10 kilos de capacidad)','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Saco de dormir','2 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Símbolo religioso (plata)','25 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Vendas para heridas (x1)','1 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Yesca y pedernal','1 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Posada modesta (pensión completa)','5 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Posada corriente (pensión completa)','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Posada lujosa (pensión completa)','3 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Comida corriente','2 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Botella de vino','2 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Jarra de cerveza','1 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Armadura de cuero','5 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Armadura de anillas','30 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Armadura de mallas','75 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Armadura de placas','100 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Escudo','15 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Hacha de Batalla','5 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Porra','0 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Daga','2 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Martillo de Guerra','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Maza Pesada','10 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Lanza','1 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Bastón (a dos manos)','0 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Espada Larga','15 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Espada Corta','8 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Espada a dos manos','30 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Arco','15 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Ballesta ligera','12 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Daga','2 mo');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Honda','2 mp');
+INSERT INTO EQUIPMENT (NAME,PRICE)
+VALUES ('Lanza','1 mo');
+INSERT INTO ARMOR VALUES (32,2);
+INSERT INTO ARMOR VALUES (33,3);
+INSERT INTO ARMOR VALUES (34,4);
+INSERT INTO ARMOR VALUES (35,6);
+INSERT INTO ARMOR VALUES (36,1);
+INSERT INTO WEAPON VALUES (37, '1d4');
+INSERT INTO WEAPON VALUES (38, '1d4');
+INSERT INTO WEAPON VALUES (39, '1d4+1');
+INSERT INTO WEAPON VALUES (40, '1d6');
+INSERT INTO WEAPON VALUES (41, '1d6');
+INSERT INTO WEAPON VALUES (42, '1d6');
+INSERT INTO WEAPON VALUES (43, '1d8');
+INSERT INTO WEAPON VALUES (44, '1d6');
+INSERT INTO WEAPON VALUES (45, '1d10');
+INSERT INTO WEAPON VALUES (46, '1d6');
+INSERT INTO WEAPON VALUES (47, '1d4+1');
+INSERT INTO WEAPON VALUES (48, '1d4');
+INSERT INTO WEAPON VALUES (49, '1d4');
+INSERT INTO WEAPON VALUES (50, '1d6');
+INSERT INTO RANGED VALUES (46, 2, '60 metros');
+INSERT INTO RANGED VALUES (47, 1, '70 metros');
+INSERT INTO RANGED VALUES (48, 1, '4 metros');
+INSERT INTO RANGED VALUES (49, 1, '20 metros');
+INSERT INTO RANGED VALUES (50, 1, '8 metros');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('De alguna forma tienes facilidad para adaptarte a cualquier ambiente y entorno. Ganas un modificador adicional de +1 a tu rasgo de Instintos.','Adaptable');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes ventaja en cualquier prueba de Erudición relacionada con el trabajo sobre piedra. También puedes hacer una prueba de Alerta para detectar trampas hechas sobre roca.','Afín a la Piedra');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Al abatir a un enemigo puedes realizar inmediatamente otro ataque. De esta forma puedes encadenar tantos ataques EXTRA como nivel tengas.','Ataques Múltiples');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes un bono adicional de +1 en todos los ataques de proyectiles.','Certero');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes ventaja en las pruebas de Manipulación que requieran precisión (por ejemplo abrir cerraduras o sustraer las posesiones de otra persona).','Dedos Ágiles');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes ventaja en las pruebas de Subterfugio basadas en moverse en silencio y en ocultarse en las sombras. Añade 1d6 al daño cuando ataques a un blanco desprevenido.','Emboscar');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes ventaja en las pruebas de Subterfugio relacionadas con moverse en silencio.','Escurridizo');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Vives la vida con intensidad. Ganas un trasfondo adicional.','Ímpetu Emprendedor');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Ves en la oscuridad hasta 20 metros. La visión se basa en el calor desprendido por los objetos.','Infravisión');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Sustituye X por: arma de mano y escudo o dos armas de mano o armas a dos manos o armas de proyectiles. Peleando de esa forma ganas +1 al ataque y al daño.','Lucha con X');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Puedes gastar puntos de poder (Pod) para realizar conjuros.','Sensibilidad Mágica');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Puedes convertir 3 puntos de vida en un punto de poder (Pod).','Transferir Esencia');
+INSERT INTO TALENT (DESCRIPTION,NAME)
+VALUES ('Tienes ventaja en las pruebas de Alerta basadas en la vista.','Vista Aguda');
+INSERT INTO RACE (MOVEMENT,NAME)
+VALUES (12,'Elfo');
+INSERT INTO RACE (MOVEMENT,NAME)
+VALUES (9,'Mediano');
+INSERT INTO RACE (MOVEMENT,NAME)
+VALUES (9,'Enano');
+INSERT INTO RACE (MOVEMENT,NAME)
+VALUES (12,'Humano');
+INSERT INTO CLASS (DA,NAME)
+VALUES ('1d8','Guerrero');
+INSERT INTO CLASS (DA,NAME)
+VALUES ('1d4','Hechicero');
+INSERT INTO CLASS (DA,NAME)
+VALUES ('1d6','Bribón');
+INSERT INTO RACE_TALENT VALUES (1,9);
+INSERT INTO RACE_TALENT VALUES (1,13);
+INSERT INTO RACE_TALENT VALUES (2,4);
+INSERT INTO RACE_TALENT VALUES (2,7);
+INSERT INTO RACE_TALENT VALUES (3,2);
+INSERT INTO RACE_TALENT VALUES (3,9);
+INSERT INTO RACE_TALENT VALUES (4,8);
+INSERT INTO RACE_TALENT VALUES (4,1);
+INSERT INTO CLASS_TALENT VALUES (1,3);
+INSERT INTO CLASS_TALENT VALUES (1,10);
+INSERT INTO CLASS_TALENT VALUES (2,11);
+INSERT INTO CLASS_TALENT VALUES (2,12);
+INSERT INTO CLASS_TALENT VALUES (3,6);
+INSERT INTO CLASS_TALENT VALUES (3,5);
+
